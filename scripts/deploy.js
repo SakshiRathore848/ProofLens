034aa8905e2a24ea7295595183ccd3884b9e1844
SakshiRathore848 <sakshirathore848@gmail.com>
2025-10-01 22:11:13 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,14 @@
+async function main() {
+  const ProofLens = await ethers.getContractFactory("ProofLens");
+  const proofLens = await ProofLens.deploy();
 
+  await proofLens.deployed();
+  console.log("ProofLens deployed to:", proofLens.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
