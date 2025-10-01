bc891a8ed34f21861120ac93583e4096a4a6c7ec
SakshiRathore848 <sakshirathore848@gmail.com>
2025-10-01 22:17:06 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,13 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
