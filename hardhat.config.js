b6ad9e7c3be3162ff75f351abf0c77bc7d9f6ec0
SakshiRathore848 <sakshi.rathore@geeksofgurukul.com>
2025-11-05 14:10:08 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,23 @@
-require("@nomiclabs/hardhat-ethers"); 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.17",
   networks: {
-    hardhat: {},
-    sepolia: {
-      url: process.env.ALCHEMY_API_URL || "",
+    coreTestnet: {
+      url: "https://rpc.test2.btcs.network",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1114,
     },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
 };
